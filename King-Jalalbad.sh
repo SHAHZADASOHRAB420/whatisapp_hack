9beeb83c736f5cd80a1fd51f0e074096e370a0f0
SHAHZADASOHRAB420 <71369025+SHAHZADASOHRAB420@users.noreply.github.com>
2021-01-01 03:04:25 +0430

    Add files via upload

---
+++ King-Jalalbad.sh
@@ -0,0 +1,26 @@
+
+
+
+#Mod by        : Sohrab Jan
+#Tool Unfaedah : king;"m"
+
+clear
+
+                                          
+
+ echo $mer"               Whatisapp Hack By king Jalalbad "
+
+
+ echo ""
+ echo -n $cy" Enter Whatsapp Number for hack: "
+ read t
+ echo $pur"<•=============King============•>"
+ echo -n $cy" Click on verification on ok Whatsapp and then tipe ok: "
+ read t
+ echo $pur"<•===========SoHrAb Jan============•>"
+ echo $me "Verification Code loading "
+ sleep 10
+ 
+ #!/bin/bash
+
+ echo Code:$((RANDOM + 456907))
